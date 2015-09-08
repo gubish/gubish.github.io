a1eb3c1b58eb346ab208bb9c66fb3698903da463
docccdev <doccc.dev@gmail.com>
2015-09-08 14:56:27 +0300

    add mobile responsive

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,4 @@
 var gulp            = require('gulp'),
-    minifyCss       = require('gulp-minify-css'),
     sass            = require('gulp-sass'),
     autoprefixer    = require('gulp-autoprefixer'),
     concat          = require('gulp-concat');
@@ -9,7 +8,6 @@ gulp.task('default', function() {
         .pipe(sass())
         .pipe(concat('common.css'))
         .pipe(autoprefixer('last 5 version', '> 1%', 'ie 8'))
-        .pipe(minifyCss({compatibility: 'ie8'}))
         .pipe(gulp.dest('css/'));
 });
 
